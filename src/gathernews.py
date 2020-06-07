00e2dc0ec2af9111738461ddc1890733ad6686fe
aac190002 <66142794+aac190002@users.noreply.github.com>
2020-06-06 23:08:52 -0500

    Added get articles function

--- src/gathernews.py
+++ src/gathernews.py
@@ -8,6 +8,12 @@ ATCM 3311.0U1
 import nltk
 import newspaper
 
+SOURCES = [
+    u"https://arstechnica.com/",
+    u"https://www.mentalfloss.com/",
+    u"https://www.wired.com/"
+]
+
 def user_keyword ():
     """
     Ask user for a keyword
@@ -16,11 +22,23 @@ def user_keyword ():
     userinput = input("Please enter any keyword (press enter to continue): ")
     return userinput
 
+def get_articles():
+    """
+    Gets all of the latest articles from SOURCES
+    :return: List of Articles
+    """
+    newspapers = [newspaper.build(url, memoize_articles=False) for url in SOURCES]
+    articles = [][:]
+    for paper in newspapers:
+        articles.extend(paper.articles)
+    return articles
+
 if __name__ == '__main__':
     # Ask user for keyword input
     keyword = user_keyword()
 
     # Get articles
+    articles = get_articles()
 
     # Filter by keyword
 
