8c0ab640268ef37e1e68c53b3745a671020768c4
aac190002 <66142794+aac190002@users.noreply.github.com>
2020-06-06 22:32:33 -0500

    Added user_keyword function

--- src/gathernews.py
+++ src/gathernews.py
@@ -1,2 +1,29 @@
+"""
+Asmita Chitale
+Assignment 01
+ATCM 3311.0U1
+06/11/2020
+"""
+
 import nltk
 import newspaper
+
+def user_keyword ():
+    """
+    Ask user for a keyword
+    :return: the keyword
+    """
+    userinput = input("Enter a keyword: ")
+    return userinput
+
+if __name__ == '__main__':
+    # Ask user for keyword input
+    keyword = user_keyword()
+
+    # Get articles
+
+    # Filter by keyword
+
+    # Extra feature
+
+    pass
